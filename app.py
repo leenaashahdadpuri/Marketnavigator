793df3b9ce5dea1ffa8dba96efb540e14cb57140
leenaashahdadpuri <leenaashahdadpuri@gmail.com>
2026-06-04 23:14:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+import streamlit as st
+
+st.set_page_config(
+    page_title="Store Inventory System"
+)
+
+st.title("Store Inventory Management")
+
+st.write(
+    "Select a page from the sidebar."
+)
